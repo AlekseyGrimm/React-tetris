6a27a5be20857ef854991238353640428af495a1
AlekseyGrimm <stalpetc@gmail.com>
2021-03-03 15:08:50 +0300

    feat: update code and add useEffect, useCallback

--- game/src/components/Tetris.js
+++ game/src/components/Tetris.js
@@ -17,18 +17,45 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
+  const [player,  updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
-  console.log('re-render');
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 });
+  }
 
- 
+  const startGame = () => {
+    console.log("test")
+    // Reset everything
+    setStage(createStage());
+    resetPlayer();
+  }
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false })
+  }
+
+  const dropPlayer = () => {
+    drop();
+  }
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1);
+      } else if (keyCode === 39) {
+        movePlayer(1);
+      } else if (keyCode === 40) {
+        dropPlayer();
+      }
+    }
+  }
 
   return (
     <div>
       <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
         <StyledTetris>
-          <Stage stage={createStage()} />
+          <Stage stage={stage} />
           <aside>
             {gameOver ? (
               <Display gameOver={gameOver} text="Game Over" />
@@ -39,7 +66,7 @@ const Tetris = () => {
                   <Display text="Level" />
                 </div>
               )}
-            <StartButton />
+            <StartButton callback={startGame} />
           </aside>
         </StyledTetris>
       </StyledTetrisWrapper>
